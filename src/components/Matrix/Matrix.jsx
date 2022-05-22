48bbcba2bb5ddf03acd219c4c1e22688220a3b52
sntgreale <sntg.reale@gmail.com>
2022-05-22 00:38:03 -0300

    p - Prettier code. Optimization components.

--- src/components/Matrix/Matrix.jsx
+++ src/components/Matrix/Matrix.jsx
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import * as PropType from 'prop-types';
 import Box from '../Box/Box';
+import handleBoxAction from '../../functions/handleBoxActions';
 import './Matrix.scss';
 
 /**
@@ -9,6 +10,31 @@ import './Matrix.scss';
  */
 
 const Matrix = ({ data }) => {
+  const [newData, setNewData] = useState(data);
+
+  const handleClickLeftOnBox = (dataFromChildren) => {
+    // Call function to check if Box is not already showed.
+    //* If isDisabled = true, this is not necessary.
+    //* Because the value corresponding to the box is being displayed
+    //* but the button is disabled to trigger any function.
+    // Call the function to check state of Box AFTER unhide their value.
+    //* If it is a mine, it is necessary to proceed differently than if it were any other number.
+    // Call the function to unhide the Box value.
+    //* Update isHidden state.
+    // Call the function to update state.
+    console.log('Left click on Matrix');
+  };
+
+  const handleClickRightOnBox = (dataFromChildren) => {
+    // TODO Logic for right click
+    /*
+     * When the user right clicks,
+     * it is necessary to update the state.userChoice ( nothing -> flag -> question ).
+     * But do not show the number (or mine).
+     */
+    console.log('Right click on Matrix');
+  };
+
   return (
     <div
       className='matrix'
@@ -17,11 +43,19 @@ const Matrix = ({ data }) => {
       }}
     >
       <div className='matrix-container'>
-        {data.map((row) => (
-          <div className='matrix-rows' key={row.position.row}>
+        {newData.map((row) => (
+          <div className='matrix-rows' key={row[0].position.row}>
             {row.map((col) => (
               <div className='matrix-columns' key={col.position.column}>
-                <Box data={col} />
+                <Box
+                  data={col}
+                  handleClickLeftOnBox={(boxInfo) => {
+                    handleClickLeftOnBox(boxInfo);
+                  }}
+                  handleClickRightOnBox={(boxInfo) => {
+                    handleClickRightOnBox(boxInfo);
+                  }}
+                />
               </div>
             ))}
           </div>
